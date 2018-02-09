f2023c49cbfb7539ca7b5901d25d930ccf8d98c9
Robert Forsyth <rhfk3@schls.albco>
2018-02-09 15:27:37 -0500

    Added a new method

--- FinalMethodProject/FinalMethodProject.ino
+++ FinalMethodProject/FinalMethodProject.ino
@@ -1,3 +1,10 @@
+//Jingle Bells stuff
+int jingleBells[] = {0, 2, 2, 0, 0, 2, 2, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 2, 2, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 2, 2, 0, 0, 3, 3, 0, 0, 2, 2, 0, 0, 1, 1, 0, 0, 0, 0, 0};
+int jbBowMovement[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int jbtempo = 100;
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -5,11 +12,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
+    beginPlaying(jingleBells, jbBowMovement, jbtempo);
 }
 
+//this method works by taking a bow movement array, a song array, and a tempo int to run the song
 
 void beginPlaying(int bowMove[], int notes[], int tempo){
-//this method works by taking a bow movement array, a song array, and a tempo int to run the song
 
 }
