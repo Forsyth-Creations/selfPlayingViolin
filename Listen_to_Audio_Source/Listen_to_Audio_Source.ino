e499743e4917e59bf2beb126ce4d113c2e1e591a
Robert Forsyth <rhfk3@schls.albco>
2018-02-12 14:15:27 -0500

    test

--- Listen_to_Audio_Source/Listen_to_Audio_Source.ino
+++ Listen_to_Audio_Source/Listen_to_Audio_Source.ino
@@ -1,9 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.print("Signal 1");
+Serial.println(analogRead(A0));
+  Serial.print("Signal 2");
+Serial.println(analogRead(A1));
+delay(250);
 }
