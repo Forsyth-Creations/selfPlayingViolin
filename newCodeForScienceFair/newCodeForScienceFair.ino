40ae091f9e1633f43c4de35f013da7602f263821
Robert Forsyth <rhfk3@schls.albco>
2018-03-06 21:12:58 -0500

    Final Code

--- newCodeForScienceFair/newCodeForScienceFair.ino
+++ newCodeForScienceFair/newCodeForScienceFair.ino
@@ -10,7 +10,19 @@ int upServo2 = 0;
 int upServo3 = 180;
 int upServo4 = 150;
 
+#define led1Pin A0
+#define led2Pin A1
+#define led3Pin A2
+#define led4Pin A3
+#define buttonPin 3
+#define servoPin1 7
+#define servoPin2 6
+#define servoPin3 5
+#define servoPin4 4
+
+
 void setup() {
+  Serial.begin(9600);
   servo1.attach(7);
   servo2.attach(6);
   servo3.attach(5);
@@ -20,19 +32,7 @@ void setup() {
 }
 
 void loop() {
-
-  moveServo(4, true);
-  delay(600);
-  moveServo(3, true);
-  delay(600);
-  moveServo(2, true);
-  delay(600);
-  moveServo(1, true);
-  delay(600);
-  allUp();
-  delay(2000);
-
-
+  oneOctaveGScale();
 }
 
 void moveServo(int whatServo, boolean upOrDown) {
@@ -42,29 +42,39 @@ void moveServo(int whatServo, boolean upOrDown) {
     case 1:
       i = whichWay(upOrDown);
       servo1.write(upServo1 + (50 * i * -1));
+      allOffExcept(whatServo);
       break;
     case 2:
       i = whichWay(upOrDown);
       servo2.write(upServo2 + (90 * i * -1 ));
+      allOffExcept(whatServo);
       break;
     case 3:
       i = whichWay(upOrDown);
       servo3.write(upServo3 + (100 * i));
+      allOffExcept(whatServo);
       break;
     case 4:
       i = whichWay(upOrDown);
       servo4.write(upServo4 + (50 * i));
+      allOffExcept(whatServo);
       break;
     default:
       allUp();
   }
+  delay(160);
+  allUpExcept(whatServo);
 }
 
 void allUp() {
-  servo1.write(70);
-  servo2.write(0);
-  servo3.write(180);
-  servo4.write(150);
+  servo1.write(upServo1);
+  servo2.write(upServo2);
+  servo3.write(upServo3);
+  servo4.write(upServo4);
+  analogWrite(led1Pin, 0);
+  analogWrite(led2Pin, 0);
+  analogWrite(led3Pin, 0);
+  analogWrite(led4Pin, 0);
 }
 
 int whichWay(boolean doYouKnowDaWay) {
@@ -76,5 +86,193 @@ int whichWay(boolean doYouKnowDaWay) {
     x = 1;
   }
   return x;
+}
+
+void allUpExcept(int servoNumber) {
+  switch (servoNumber) {
+    case 1:
+      servo2.write(upServo2);
+      servo3.write(upServo3);
+      servo4.write(upServo4);
+      break;
+    case 2:
+      servo1.write(upServo1);
+      servo3.write(upServo3);
+      servo4.write(upServo4);
+      break;
+    case 3:
+      servo1.write(upServo1);
+      servo2.write(upServo2);
+      servo4.write(upServo4);
+      break;
+    case 4:
+      servo1.write(upServo1);
+      servo2.write(upServo2);
+      servo3.write(upServo3);
+      break;
+    default:
+      allUp();
+  }
+
+}
+
+void playNote(int stringNumber, char note, int accidental) {
+  //The violin strings are numbered left to right (G = 4, D = 3, A = 2, E = 1)
+  //any input on the accidental should be either 1 (flat), 2 (Natural), or 3 (Sharp)
+  switch (stringNumber) {
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      if (note == 'D' && accidental == 2) {
+        moveServo(0, false);
+        Serial.print("D string, note ");
+        Serial.println(note);
+      }
+      if (note == 'E' && accidental == 2) {
+        moveServo(1, true);
+        Serial.print("D string, note ");
+        Serial.println(note);
+      }
+      if (note == 'F' && accidental == 2) {
+        moveServo(2, true);
+        Serial.print("D string, note ");
+        Serial.println(note);
+      }
+      if (note == 'F' && accidental == 3) {
+        moveServo(3, true);
+        Serial.print("D string, note ");
+        Serial.println(note);
+      }
+      if (note == 'G' && accidental == 2) {
+        moveServo(4, true);
+        Serial.print("D string, note ");
+        Serial.println(note);
+      }
+
+      break;
+    case 4:
+      if (note == 'G' && accidental == 2) {
+        moveServo(0, false);
+        Serial.print("G string, note ");
+        Serial.println(note);
+      }
+      if (note == 'A' && accidental == 2) {
+        moveServo(1, true);
+        Serial.print("G string, note ");
+        Serial.println(note);
+      }
+      if (note == 'B' && accidental == 2) {
+        moveServo(3, true);
+        Serial.print("G string, note ");
+        Serial.println(note);
+      }
+      if (note == 'B' && accidental == 1) {
+        moveServo(2, true);
+        Serial.print("G string, note ");
+        Serial.println(note);
+      }
+      if (note == 'C' && accidental == 2) {
+        moveServo(4, true);
+        Serial.print("G string, note ");
+        Serial.println(note);
+      }
+
+      break;
+
+  }
+}
+
+void oneOctaveGScale() {
+  Serial.println("Starting Scale");
+  playNote(4, 'G', 2);
+  pauseFor(1000);
+  playNote(4, 'A', 2);
+  pauseFor(1000);
+  playNote(4, 'B', 2);
+  pauseFor(1000);
+  playNote(4, 'C', 2);
+  pauseFor(1000);
+  playNote(3, 'D', 2);
+  pauseFor(1000);
+  playNote(3, 'E', 2);
+  pauseFor(1000);
+  playNote(3, 'F', 3);
+  pauseFor(1000);
+  playNote(3, 'G', 2);
+  pauseFor(1000);
+  //back down now
+  playNote(3, 'G', 2);
+  pauseFor(1000);
+  playNote(3, 'F', 3);
+  pauseFor(1000);
+  playNote(3, 'E', 2);
+  pauseFor(1000);
+  playNote(3, 'D', 2);
+  pauseFor(1000);
+  playNote(4, 'C', 2);
+  pauseFor(1000);
+  playNote(4, 'B', 2);
+  pauseFor(1000);
+  playNote(4, 'A', 2);
+  pauseFor(1000);
+  playNote(4, 'G', 2);
+  pauseFor(1000);
+}
+
+void setAllToNinety() {
+  servo1.write(105);
+  servo2.write(70);
+  servo3.write(90);
+  servo4.write(120);
+}
+
+void allOffExcept(int ledNumber) {
+  switch (ledNumber) {
+    case 1:
+      analogWrite(led1Pin, 200);
+      analogWrite(led2Pin, 0);
+      analogWrite(led3Pin, 0);
+      analogWrite(led4Pin, 0);
+      break;
+    case 2:
+      analogWrite(led1Pin, 0);
+      analogWrite(led2Pin, 200);
+      analogWrite(led3Pin, 0);
+      analogWrite(led4Pin, 0);
+      break;
+    case 3:
+      analogWrite(led1Pin, 0);
+      analogWrite(led2Pin, 0);
+      analogWrite(led3Pin, 200);
+      analogWrite(led4Pin, 0);
+      break;
+    case 4:
+      analogWrite(led1Pin, 0);
+      analogWrite(led2Pin, 0);
+      analogWrite(led3Pin, 0);
+      analogWrite(led4Pin, 200);
+      break;
+  }
+
+}
+
+void pauseFor(int delayTime) {
+
+  for (int i = 0; i < delayTime/100; i++) {
+    delay(100);
+    if (digitalRead(buttonPin) == HIGH) {
+      Serial.println("BUTON PRESSED");
+      delay(100);
+      do {
+        delay(10);
+      } while (digitalRead(buttonPin) == LOW);
+     
+    }
+
+  }
+return;
+
 }
 
